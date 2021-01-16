aecc255150efb3f8610920e8b01d457e5089892f
joejoy <joejoy456999@gmail.com>
2021-01-16 12:03:47 +0530

    COMPLETED

--- file.sql
+++ file.sql
@@ -2,3 +2,19 @@ CREATE TABLE tableone(
     Name VARCHAR(200) NOT NULL,
     Address VARCHAR(500) NOT NULL
 );
+
+CREATE TABLE animal_details(
+    id INT NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    Scientific_name VARCHAR(500) NOT NULL,
+    birthdate date,
+    time datetime
+);
+
+CREATE TABLE date_times(
+    id int unsigned NOT NULL auto_increment,
+    username VARCHAR(200) NOT NULL,
+    dob_timestamp TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
